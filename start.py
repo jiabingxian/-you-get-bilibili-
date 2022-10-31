fd8f9921dfbfbc6f71e9667508416b8123c974ef
jiabingxian <100575580+jiabingxian@users.noreply.github.com>
2022-10-31 09:35:58 +0800

    Update start.py

--- start.py
+++ start.py
@@ -3,6 +3,8 @@ from you_get import (  # 需安装you-get这个轮子
 )
 p = "完成!"
 d = "开始下载"
+print("欢迎使用视频下载器")
+print("报告BUG：https://github.com/jiabingxian/download-bilibili/issues")
 url = str(input("视频URL:"))
 print(d)
 common.any_download_playlist(url, output_dir=".\\video", merge=True, playlists=True)
